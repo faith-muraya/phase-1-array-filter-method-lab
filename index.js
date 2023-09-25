18c4846485beea03b673b75c598068fb99a70f0e
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2023-09-25 20:33:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, driver) {
+    return drivers.filter(
+        (driverName) => driverName.toLowerCase() == driver.toLowerCase()
+    );
+}
+
+function fuzzyMatch(drivers, driver) {
+    return drivers.filter((driverName) => 
+    driverName.toLowerCase().startsWith(driver.toLowerCase())
+    );
+}
+
+function matchName(drivers, driver) {
+    return drivers.filter(
+        (driverElement) => driverElement.name.toLowerCase() == driver.toLowerCase()
+    );
+}
